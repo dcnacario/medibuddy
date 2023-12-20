40601193d29b66a9da2a35b74df0cc9d8ef170ce
Danmar Cornelio Nacario <dcnacario08@gmail.com>
2023-12-20 09:54:34 +0800

    add: validation for schedule and fetching data

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Kreait\Firebase\Contract\Database;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Carbon;
 
 class ScheduleController extends Controller
 {
@@ -17,13 +18,40 @@ class ScheduleController extends Controller
 
     public function create()
     {
-        return view('schedules.create');
-    }
-    public function index()
-    {
-        $schedules = $this->getSchedulesFromFirebase();
+        try {
+            // Get the current date
+            $currentDate = Carbon::now()->format('Y-m-d');
 
-        return view('schedules.index', compact('schedules'));
+            // Fetch schedules for the current date from Firebase
+            $scheduleForToday = $this->formatTime($this->getSchedulesForDate($currentDate));
+
+            $allSchedules = $this->getSchedulesFromFirebase();
+
+            return view('schedules.create', compact('scheduleForToday', 'allSchedules'));
+        } catch (\Exception $e) {
+            Log::error('Firebase Error:', ['message' => $e->getMessage(), 'trace' => $e->getTrace()]);
+            // Handle the error appropriately (e.g., return an error response)
+            return redirect()->back()->with('error', 'Error occurred while fetching today\'s schedule');
+        }
+    }
+
+    private function getSchedulesForDate($date)
+    {
+        $schedule = [];
+
+        // Assuming you store schedules under a 'schedules' node in Firebase
+        $snapshot = $this->database->getReference("schedules/$date")->getSnapshot();
+
+        if ($snapshot->exists()) {
+            foreach ($snapshot->getValue() as $timeSlot) {
+                $schedule[] = [
+                    'label' => $timeSlot['label'] ?? null,
+                    'time' => $timeSlot['time'] ?? null,
+                ];
+            }
+        }
+
+        return $schedule;
     }
 
     private function getSchedulesFromFirebase()
@@ -36,11 +64,22 @@ class ScheduleController extends Controller
         foreach ($snapshot->getValue() as $date => $timeSlots) {
             $schedule = new \stdClass();
             $schedule->date = $date;
-            $schedule->time_slots = $timeSlots;
+            $schedule->time_slots = $this->formatTime($timeSlots);
             $schedules[] = $schedule;
         }
 
         return $schedules;
+    }
+
+    private function formatTime($timeSlots)
+    {
+        foreach ($timeSlots as &$timeSlot) {
+            if (isset($timeSlot['time'])) {
+                $timeSlot['time'] = Carbon::createFromFormat('H:i', $timeSlot['time'])->format('h:i A');
+            }
+        }
+
+        return $timeSlots;
     }
 
 
@@ -48,38 +87,71 @@ class ScheduleController extends Controller
     {
         try {
             $request->validate([
-                'date' => 'required|date',
+                'date' => [
+                    'required',
+                    'date',
+                    'after_or_equal:' . Carbon::now()->format('Y-m-d'),
+                    function ($attribute, $value, $fail) use ($request) {
+                        // Check uniqueness in Firebase RTDB
+                        $date = $request->input('date');
+                        $existingData = $this->database->getReference("schedules/$date")->getSnapshot()->exists();
+
+                        if ($existingData) {
+                            $fail('The ' . $attribute . ' has already been taken.');
+                        }
+                    },
+                ],
                 'time_slots' => 'required|array',
                 'time_slots.*' => 'nullable|date_format:H:i',
             ]);
 
+
             $date = $request->input('date');
             $timeSlots = $request->input('time_slots');
+            $labelTimeSlots = $request->input('label_time_slots');
 
-            // Retrieve the existing time slots from Firebase
-            $existingTimeSlots = $this->database->getReference("schedules/$date")->getSnapshot()->getValue();
+            // Check if the schedule for the given date already exists
+            $scheduleExists = $this->database->getReference("schedules/$date")->getSnapshot()->exists();
 
-            // If there are existing time slots, update them with the new ones
-            if ($existingTimeSlots) {
-                // Merge existing time slots with the new ones
-                $updatedTimeSlots = $existingTimeSlots;
-                foreach ($timeSlots as $key => $slot) {
-                    $updatedTimeSlots["time_slot_" . ($key + 1)] = $slot;
-                }
-            } else {
-                // If no existing time slots, use the new ones directly with specified names
-                $updatedTimeSlots = array_combine(
-                    array_map(function ($key) {
-                        return "time_slot_$key";
-                    }, range(1, count($timeSlots))),
-                    $timeSlots
-                );
+            // Check if the selected date is today
+            $selectedDate = Carbon::parse($request->input('date'))->format('Y-m-d');
+            $today = Carbon::now()->format('Y-m-d');
+
+            if ($selectedDate == $today) {
+                // Handle the case where the selected date is today
+                return redirect()->route('schedules.create')->with('error', 'You cannot schedule for today. Please select a future date.');
             }
 
-            // Store the updated schedule in Firebase
-            $this->database->getReference("schedules/$date")->set($updatedTimeSlots);
+            // Check if the schedule for the selected date already exists
+            $scheduleExists = $this->database->getReference("schedules/$selectedDate")->getSnapshot()->exists();
 
-            return redirect()->route('schedules.create')->with('message', 'Schedule updated successfully');
+            if ($scheduleExists) {
+                // Handle the case where the schedule already exists
+                return redirect()->route('schedules.create')->with('error', 'Schedule already exists for the selected date');
+            } else {
+                // Continue with the code to store the schedule in Firebase
+
+                // Retrieve the existing time slots and labels from Firebase
+                $existingData = $this->database->getReference("schedules/$date")->getSnapshot()->getValue();
+
+                // If there are existing time slots, update them with the new ones
+                $updatedData = $existingData ?? [];
+
+                foreach ($timeSlots as $key => $slot) {
+                    $timeSlotKey = "time_slot_" . ($key + 1);
+                    $updatedData[$timeSlotKey]['time'] = $slot;
+
+                    // Check if a label exists for this time slot
+                    if (isset($labelTimeSlots[$key])) {
+                        $updatedData[$timeSlotKey]['label'] = $labelTimeSlots[$key];
+                    }
+                }
+
+                // Store the updated schedule in Firebase
+                $this->database->getReference("schedules/$date")->set($updatedData);
+
+                return redirect()->route('schedules.create')->with('message', 'Schedule updated successfully');
+            }
         } catch (\Exception $e) {
             Log::error('Firebase Error:', ['message' => $e->getMessage(), 'trace' => $e->getTrace()]);
             // Handle the error appropriately (e.g., return an error response)
--- resources/views/schedules/create.blade.php
+++ resources/views/schedules/create.blade.php
@@ -3,6 +3,7 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,174 +11,200 @@
     <title>Create Schedule</title>
 </head>
 <style>
-    *{
+    * {
         font-family: 'Poppins';
-        margin:0;
-        padding:0;
+        margin: 0;
+        padding: 0;
     }
-    body{
-        background-image: url('{{ asset('images/lastcurve.png') }}'); 
-        background-size: cover; 
+
+    body {
+        background-image: url('{{ asset(' images/lastcurve.png') }}');
+        background-size: cover;
         background-repeat: no-repeat;
     }
-    .navbar{
-        background:#bfdbffff;
+
+    .navbar {
+        background: #bfdbffff;
     }
-    .navbar img{
-        margin:40px 40px 0px;
-        width:22%;
+
+    .navbar img {
+        margin: 40px 40px 0px;
+        width: 22%;
     }
-    .center{
-        background:#ffffffff;
-        border-radius:15px;
-        margin:60px 530px;
+
+    .center {
+        background: #ffffffff;
+        border-radius: 15px;
+        margin: 60px 530px;
         box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
     }
-    .inputs{
-        padding:30px 30px;
+
+    .inputs {
+        padding: 30px 30px;
     }
-    .inputs label{
-        font-size:20px;
+
+    .inputs label {
+        font-size: 20px;
     }
-    .inputs input{
-        background:#bfdbffff;
-        border-radius:3px;
-        border:2px solid rgba(0, 0, 0, 0.5);
-        height:30px;
-        width:100%;
+
+    .inputs input {
+        background: #bfdbffff;
+        border-radius: 3px;
+        border: 2px solid rgba(0, 0, 0, 0.5);
+        height: 30px;
+        width: 100%;
     }
-    .inputs .put{
-        margin-bottom:10px;
+
+    .inputs .put {
+        margin-bottom: 10px;
     }
-    .inputs .puto{
-        margin-bottom:5px;
+
+    .inputs .puto {
+        margin-bottom: 5px;
     }
-    .title{
-        text-align:center;
-        margin:20px 250px;
+
+    .title {
+        text-align: center;
+        margin: 20px 250px;
     }
-    .title h1{
-        color:#001f3fff;
-        font-size:35px;
+
+    .title h1 {
+        color: #001f3fff;
+        font-size: 35px;
         /* text-transform: uppercase; */
     }
-    .sub-btn{
-        display:flex;
-        justify-content:center;
-        margin-top:5px;
+
+    .sub-btn {
+        display: flex;
+        justify-content: center;
+        margin-top: 5px;
     }
-    .sub-btn button{
-        color:white;
-        cursor:pointer;
-        border-radius:5px;
-        font-size:18px;
-        border:none;
-        background:rgb(1, 67, 151, 0.9);
-        height:40px;
-        padding:5px 10px;
+
+    .sub-btn button {
+        color: white;
+        cursor: pointer;
+        border-radius: 5px;
+        font-size: 18px;
+        border: none;
+        background: rgb(1, 67, 151, 0.9);
+        height: 40px;
+        padding: 5px 10px;
         transition: background 0.3s;
     }
+
     .sub-btn button:hover {
-        background: #bfdbffff; 
-        color:black;
-        border:2px solid rgba(0, 0, 0, 0.5);
-    }
-    .setTime{
-        font-size:20px;
-        margin:0px 40px;
-    }
-    .oten{
-        padding:10px;
-    }
-    .bilat{
-        background:rgb(0, 31, 63, 1);
-        color:#ffffffff;
-        margin:10px 300px;
-        padding:5px 15px;
-        display:flex;
-        justify-content:space-between;
-        border-radius:10px;
-        font-size:10px;
+        background: #bfdbffff;
+        color: black;
+        border: 2px solid rgba(0, 0, 0, 0.5);
     }
 
-     /* Media query for mobile screens */
-     @media (max-width: 768px) {
-            body{
-                background-image: url('{{ asset('images/lastMobile.png') }}');
-                background-position: center;
-                background-repeat: no-repeat;
-            }
-            .navbar img{
-                margin:40px 40px 0px;
-                width:55%;
-            }
-            .title{
-                text-align:center;
-                padding:10px;
-                margin:30px 20px 0px 20px;
-            }
-            .title h1{
-                color:#001f3fff;
-                font-size:20px;
-            }
-            .center{
-                background:#ffffffff;
-                border-radius:15px;
-                margin:30px 35px 50px 35px;
-                box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
-            }
-            .inputs{
-                padding:20px 20px;
-            }
-            .inputs label{
-                font-size:20px;
-            }
-            .inputs input{
-                background:#bfdbffff;
-                border-radius:3px;
-                border:2px solid rgba(0, 0, 0, 0.5);
-                text-align:center;
-                height:40px;
-                font-size:18px;
-                width:100%;
-            }
-            .inputs .put{
-                margin-bottom:10px;
-            }
-            .inputs .puto{
-                margin-bottom:5px;
-            }
-            .inputs input[type="date"],
-            .inputs input[type="time"] {
-                width: 324px;
-                color:#001f3fff;
-                font-size:18px;
-                box-sizing: border-box;
-            }
-            .setTime{
-                font-size:13px;
-                margin:0px 40px;
-            }
-            .bilat{
-                background:rgb(0, 31, 63, 1);
-                color:#ffffffff;
-                margin:10px 30px;
-                padding:10px 15px;
-                display:flex;
-                justify-content:space-between;
-                border-radius:10px;
-                font-size:10px;
-            }
+    .setTime {
+        font-size: 20px;
+        margin: 0px 40px;
+    }
+
+    .oten {
+        padding: 10px;
+    }
+
+    .bilat {
+        background: rgb(0, 31, 63, 1);
+        color: #ffffffff;
+        margin: 10px 300px;
+        padding: 5px 15px;
+        display: flex;
+        justify-content: space-between;
+        border-radius: 10px;
+        font-size: 10px;
+    }
+
+    /* Media query for mobile screens */
+    @media (max-width: 768px) {
+        body {
+            background-image: url('{{ asset(' images/lastMobile.png') }}');
+            background-position: center;
+            background-repeat: no-repeat;
         }
 
+        .navbar img {
+            margin: 40px 40px 0px;
+            width: 55%;
+        }
+
+        .title {
+            text-align: center;
+            padding: 10px;
+            margin: 30px 20px 0px 20px;
+        }
+
+        .title h1 {
+            color: #001f3fff;
+            font-size: 20px;
+        }
+
+        .center {
+            background: #ffffffff;
+            border-radius: 15px;
+            margin: 30px 35px 50px 35px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+        }
+
+        .inputs {
+            padding: 20px 20px;
+        }
+
+        .inputs label {
+            font-size: 20px;
+        }
+
+        .inputs input {
+            background: #bfdbffff;
+            border-radius: 3px;
+            border: 2px solid rgba(0, 0, 0, 0.5);
+            text-align: center;
+            height: 40px;
+            font-size: 18px;
+            width: 100%;
+        }
+
+        .inputs .put {
+            margin-bottom: 10px;
+        }
+
+        .inputs .puto {
+            margin-bottom: 5px;
+        }
+
+        .inputs input[type="date"],
+        .inputs input[type="time"] {
+            width: 324px;
+            color: #001f3fff;
+            font-size: 18px;
+            box-sizing: border-box;
+        }
+
+        .setTime {
+            font-size: 13px;
+            margin: 0px 40px;
+        }
+
+        .bilat {
+            background: rgb(0, 31, 63, 1);
+            color: #ffffffff;
+            margin: 10px 30px;
+            padding: 10px 15px;
+            display: flex;
+            justify-content: space-between;
+            border-radius: 10px;
+            font-size: 10px;
+        }
+    }
 </style>
+
 <body>
     <div class="navbar curved-background">
         <img src="{{ asset('images/medibuddyOriginal.png') }}" alt="Logo">
     </div>
-    @if(session('message'))
-    <p>{{ session('message') }}</p>
-    @endif
     <div class="title">
         <h1>Optimize your health with a personalized medicine schedule for better adherence and well-being.</h1>
     </div>
@@ -185,24 +212,24 @@
     <div class="center">
         <div class="inputs">
             <form method="post" action="{{ route('schedules.store') }}">
+                @if(session('message'))
+                <p>{{ session('message') }}</p>
+                @endif
+                @if(session('error'))
+                <p>{{ session('error') }}</p>
+                @endif
                 @csrf
                 <label for="date">Date</label><br>
                 <input class="put" type="date" name="date" required><br>
 
-                <label for="time_slots[]">Time Slot 1</label><br>
-                <input class="puto" type="text" placeholder="Add Label"><br>
-                <input class="put" type="time" name="time_slots[]"><br>
+                @for ($i = 0; $i < 3; $i++) <label for="time_slots[]">Time Slot {{ $i + 1 }}</label><br>
+                    <input class="puto" type="text" placeholder="Add Label" name="label_time_slots[{{ $i }}]" required><br>
+                    <input class="put" type="time" name="time_slots[{{ $i }}]" required><br>
+                    @endfor
 
-                <label for="time_slots[]">Time Slot 2</label><br>
-                <input class="puto" type="text" placeholder="Add Label"><br>
-                <input class="put" type="time" name="time_slots[]"><br>
-
-                <label for="time_slots[]">Time Slot 3</label><br>
-                <input class="puto" type="text" placeholder="Add Label"><br>
-                <input class="put" type="time" name="time_slots[]"><br>
-                <div class="sub-btn">
-                    <button type="submit">Create Schedule</button>
-                </div>
+                    <div class="sub-btn">
+                        <button type="submit">Create Schedule</button>
+                    </div>
             </form>
         </div>
     </div>
@@ -210,22 +237,21 @@
 
     <!-- ----PARAS TABLE SA UPCOMMING NGA NA SET NGA TIME----- -->
     <div class="setTime">
-        <h1>Upcomming Time</h1>
+        <h1>Schedule for Today</h1>
         <hr>
     </div>
 
-    <!-- e loop diri ang data sa mga upcomming time -->
     <div class="oten">
+        @foreach ($scheduleForToday as $timeSlot)
         <div class="bilat">
             <div>
-                <!-- diri nimo e butang ang label name -->
-                <h1>Sample Label</h1>
+                <h1>{{ $timeSlot['label'] ?? 'No Label' }}</h1>
             </div>
             <div>
-                <!-- diri nimo e butang ang set nga time  -->
-                <h1>8:00 PM</h1>
+                <h1>{{ $timeSlot['time'] ?? 'No Time' }}</h1>
             </div>
         </div>
+        @endforeach
     </div>
 
 
@@ -236,18 +262,21 @@
         <hr>
     </div>
 
-    <!-- e loop diri ang data sa mga upcomming time -->
     <div class="oten">
+        @foreach ($allSchedules as $schedule)
         <div class="bilat">
             <div>
-                <!-- diri nimo e butang ang label name -->
-                <h1>Oten</h1>
+                <h1>{{ $schedule->date }}</h1>
             </div>
             <div>
-                <!-- diri nimo e butang ang set nga time  -->
-                <h1>8:00 PM</h1>
+                @foreach ($schedule->time_slots as $timeSlot)
+                <h1>{{ $timeSlot['label'] ?? 'No Label' }} - {{ $timeSlot['time'] ?? 'No Time' }}</h1>
+                @endforeach
             </div>
         </div>
+        @endforeach
     </div>
+
 </body>
+
 </html>
